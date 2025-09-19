eda8ca61447c2747a8600cd2901d8303719011ca
changraekang <storkcr@gmail.com>
2025-09-19 16:58:15 +0900

    tile typo 수정

--- src/constants/crabImages.ts
+++ src/constants/crabImages.ts
@@ -14,7 +14,7 @@ export const TILE_IMAGES = [
   "https://assets.sparkling-rae.com/crab-game/file/crab-13.webp",
   "https://assets.sparkling-rae.com/crab-game/file/crab-14.webp",
   "https://assets.sparkling-rae.com/crab-game/file/crab-15.webp",
-  "https://assets.sparkling-rae.com/crab-game/tile/crab-16.webp",
+  "https://assets.sparkling-rae.com/crab-game/file/crab-16.webp",
   "https://assets.sparkling-rae.com/crab-game/file/crab-17.webp",
   "https://assets.sparkling-rae.com/crab-game/file/crab-18.webp",
   "https://assets.sparkling-rae.com/crab-game/file/crab-19.webp",
